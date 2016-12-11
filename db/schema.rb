ff92262cce1d0c5bc89b0fbb6eeff93534bf3502
Ethan Orcutt <izzaboy306@gmail.com>
2016-12-11 17:09:59 -0600

    Added profiles table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161211204910) do
+ActiveRecord::Schema.define(version: 20161211230927) do
+
+  create_table "profiles", force: :cascade do |t|
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
