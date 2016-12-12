0ceccb3d4a676532e40b76d47067a146e58b52e6
Ethan Orcutt <izzaboy306@gmail.com>
2016-12-11 21:36:24 -0600

    Updated DB files

--- db/migrate/20161211234147_update_e_type.rb
+++
@@ -1,9 +0,0 @@
-class UpdateEType < ActiveRecord::Migration[5.0]
-  def change
-		create_table :employee_types do |t|
-    	t.string :title
-    	t.text :description
-    	t.timestamps
-    end
-  end
-end
