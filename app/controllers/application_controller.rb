6eaf7a24378704ba4be5189b0dc9336cba70d878
Ethan Orcutt <izzaboy306@gmail.com>
2016-12-15 22:09:50 -0600

    Switched to a side navbar for application. Changed devise layout. Customized login screen

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  layout :layout
 
   def after_sign_in_path_for(resource_or_scope)
   	stored_location_for(resource) || home_path
@@ -12,4 +13,9 @@ class ApplicationController < ActionController::Base
     		user_params.permit(:email, :password, :password_confirmation, :admin)
     	end
 	  end
+
+    def layout
+      # or turn layout off for every devise controller:
+      !devise_controller? && "application"
+    end
 end
