e4bad68bdf3885f34d4324bf9b7d323f858e7e05
Ethan Orcutt <izzaboy306@gmail.com>
2016-12-16 13:16:33 -0600

    Added a new test user to the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 user1 = User.create :email => "eeorcutt@uh.edu", :password => "test123", :admin => "t"
+user2 = User.create :email => "test@np.com", :password => "test123", :admin => "t"
